e4484fe6054c23993c00d3b3ce0afac4a72e9aa2
kapil <kc918062@gmail.com>
2024-09-18 15:04:22 +0530

    done

--- Assiments/src/Components/Tables.jsx
+++ Assiments/src/Components/Tables.jsx
@@ -1,56 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import Fetchdata from '../DataFetch/Fetchdata.js';
+import Fetchdata from '../DataFetch/Fetchdata.js'
 
-export default function PaginatorTemplateDemo() {
+export default function CheckboxRowSelectionDemo() {
     const [data, setData] = useState([]);
+    const [selectedProducts, setSelectedProducts] = useState(null);
+    const [rowClick, setRowClick] = useState(true);
 
-    const paginatorLeft = <Button type="button" icon="pi pi-refresh" text />;
-    const paginatorRight = <Button type="button" icon="pi pi-download" text />;
     const fetchdataval = Fetchdata(1);
 
     useEffect(() => {
         if (fetchdataval && Array.isArray(fetchdataval)) {
             setData(fetchdataval);
         }
-    }, [fetchdataval]);
+    }, [fetchdataval])
 
     return (
-        <div className="card" style={{ padding: '2px' }}>
-            <DataTable value={data} showGridlines paginator rows={5}
-                tableStyle={{ minWidth: '60rem', minHeight: '60rem' }}
-                rowsPerPageOptions={[5, 10, 25, 50]}
-                paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-                currentPageReportTemplate="{first} to {last} of {totalRecords}"
-                paginatorLeft={paginatorLeft}
-                paginatorRight={paginatorRight}>
-
-                {/* Align headers and columns properly */}
-                <Column field="title" header="Title"
-                    headerStyle={{ textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center' }}></Column>
-
-                <Column field="place_of_origin" header="Place of Origin"
-                    headerStyle={{ textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center' }}></Column>
-
-                <Column field="artist_display" header="Artist Display"
-                    headerStyle={{ textAlign: 'center' , marginLeft:"5px" }}
-                    bodyStyle={{ textAlign: 'center' }}></Column>
-
-                <Column field="inscriptions" header="Inscriptions"
-                    headerStyle={{ textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center'}}></Column>
-
-                <Column field="date_start" header="Date Start"
-                    headerStyle={{ textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center' }}></Column>
-                    
-                <Column field="date_end" header="Date End"
-                    headerStyle={{ textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center' }}></Column>
+        <div className="card">
+            <DataTable value={data} selectionMode={rowClick ? null : 'checkbox'} selection={selectedProducts} onSelectionChange={(e) => setSelectedProducts(e.value)} dataKey="id" tableStyle={{ minWidth: '50rem' }}>
+                <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+                <Column field="title" header="title"></Column>
+                <Column field="place_of_origin" header="place_of_origin"></Column>
+                <Column field="artist_display" header="artist_display"></Column>
+                <Column field="inscriptions" header="inscriptions"></Column>
+                <Column field="date_start" header="date_start"></Column>
+                <Column field="date_end" header="date_end"></Column>
             </DataTable>
         </div>
     );
