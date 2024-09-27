a94771917456cc9b95ed15447e8066ae43e16714
kapil <kc918062@gmail.com>
2024-09-27 14:37:08 +0530

    done

--- moneyconvertor/src/Components/Form.jsx
+++ moneyconvertor/src/Components/Form.jsx
@@ -6,32 +6,38 @@ const Form = () => {
     const [age, setAge] = useState('');
 
     const handformdetail = (e) => {
-        e.preventDefault(); // Prevents default form submission
+        e.preventDefault();
         const data = { name, age };
-        setformdata(data);
+        setformdata([...formdata,data]);
         console.log("Form Data Submitted: ", data);
+        alert("data Save...")
     }
+    
     return (
         <>
-            <form onSubmit={handformdetail}>
-                <label>Enter Your name</label>
-                <input 
-                    className='bg-slate-400' 
-                    placeholder='Name...' 
-                    type="text" 
-                    value={name} // Correct state variable for name
-                    onChange={(e) => setName(e.target.value)} 
-                />
-                <label>Enter Your age</label>
-                <input 
-                    type="number" 
-                    value={age} // Correct state variable for age
-                    className='bg-slate-400' 
-                    placeholder='Age...' 
-                    onChange={(e) => setAge(e.target.value)} 
-                />
-                <button className='bg-red-500 border-2 border-orange-300 p-1' type='submit'>Submit</button>
-            </form>
+            <div className='w-3/6 absolute left-72 bg-neutral-600'>
+                <form onSubmit={handformdetail} >
+                    <div className='flex flex-col justify-center'>
+                        <label className='text-white'>Enter Your name below</label>
+                        <input
+                            className='bg-slate-400'
+                            placeholder='Name...'
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <label className='text-white'>Enter Your age below</label>
+                        <input
+                            type="number"
+                            value={age}
+                            className='bg-slate-400'
+                            placeholder='Age...'
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                        <button className='bg-red-500 border-2 border-orange-300 p-1' type='submit'>Submit</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
